eb21be8e438d572a67529e24c16786302a4a8a0b
jelavice <jelavice@ethz.ch>
2021-09-02 11:22:37 +0200

    init commit

---
+++ src/mapping_node.cpp
@@ -0,0 +1,20 @@
+/*
+ * mapping_node.cpp
+ *
+ *  Created on: Sep 1, 2021
+ *      Author: jelavice
+ */
+#include <open3d/Open3D.h>
+
+
+int main(int argc, char **argv){
+
+	namespace otg = open3d::t::geometry;
+
+	otg::PointCloud cloud;
+
+
+
+	return 0;
+}
+
