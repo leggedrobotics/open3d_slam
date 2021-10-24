95761a89ff8dd9c354e15879881397fa0c57cefe
jelavice <jelavice@ethz.ch>
2021-10-24 20:56:47 +0200

    Add croppers

---
+++ m545_volumetric_mapping/include/m545_volumetric_mapping/croppers.hpp
@@ -0,0 +1,51 @@
+/*
+ * croppers.hpp
+ *
+ *  Created on: Oct 24, 2021
+ *      Author: jelavice
+ */
+
+#pragma once
+
+#include <Eigen/Dense>
+#include <vector>
+#include <open3d/geometry/PointCloud.h>
+
+namespace m545_mapping {
+
+class Cropper {
+
+public:
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+	using PointCloud = open3d::geometry::PointCloud;
+	using Indices =std::vector<size_t>;
+	Cropper() = default;
+	virtual ~Cropper() = default;
+
+
+	void setPose(const Eigen::Isometry3d &pose);
+	virtual bool isWithinVolume(const Eigen::Vector3d &p) const;
+	Indices getIndicesWithinVolume(const PointCloud &cloud) const;
+	std::shared_ptr<PointCloud> crop(const PointCloud &cloud) const;
+
+protected:
+
+	Eigen::Isometry3d pose_=Eigen::Isometry3d::Identity();
+
+};
+
+class RadiusCropper : public Cropper{
+public:
+	RadiusCropper(double radius);
+	virtual ~RadiusCropper() = default;
+
+	bool isWithinVolume(const Eigen::Vector3d &p) const final;
+
+
+private:
+	double radius_=1e6;
+
+};
+
+
+} // namespace m545_mapping
---
+++ m545_volumetric_mapping/src/croppers.cpp
@@ -0,0 +1,52 @@
+/*
+ * croppers.cpp
+ *
+ *  Created on: Oct 24, 2021
+ *      Author: jelavice
+ */
+
+#include <Eigen/Core>
+#include <vector>
+#include "m545_volumetric_mapping/croppers.hpp"
+
+namespace m545_mapping {
+
+bool Cropper::isWithinVolume(const Eigen::Vector3d &p) const {
+	return true;
+}
+
+void Cropper::setPose(const Eigen::Isometry3d &pose){
+	pose_ = pose;
+}
+
+Cropper::Indices Cropper::getIndicesWithinVolume(const PointCloud &cloud) const{
+
+	Indices idxs;
+	idxs.reserve(cloud.points_.size());
+
+	for(size_t i =0; i < cloud.points_.size(); ++i){
+		if(isWithinVolume(cloud.points_[i])){
+			idxs.push_back(i);
+		}
+	}
+
+	return idxs;
+}
+
+std::shared_ptr<Cropper::PointCloud> Cropper::crop(const PointCloud &cloud) const{
+	const auto idxsInside = getIndicesWithinVolume(cloud);
+	return cloud.SelectByIndex(idxsInside);
+}
+
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+
+RadiusCropper::RadiusCropper(double radius):radius_(radius){
+}
+
+bool RadiusCropper::isWithinVolume(const Eigen::Vector3d &p) const{
+	return (p-pose_.translation()).norm() <= radius_;
+}
+
+} // namespace m545_mapping
