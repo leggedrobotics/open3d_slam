217fd08ec53fc3a4177a57465b2f06dc99a3cc42
Juli <nubertj@ethz.ch>
2022-02-08 17:40:53 +0000

    shortened names from open3d to o3d

--- open3d_slam/include/open3d_slam/frames.hpp
+++ open3d_slam/include/open3d_slam/frames.hpp
@@ -10,10 +10,10 @@
 
 namespace o3d_slam {
 namespace frames {
-static const std::string odomFrame = "odom_open3d";
-static const std::string rangeSensorFrame = "range_sensor_open3d";
-static const std::string mapFrame = "map_open3d";
-static const std::string imageFrame = "image_frame_open3d";
+static const std::string odomFrame = "odom_o3d";
+static const std::string rangeSensorFrame = "range_sensor_o3d";
+static const std::string mapFrame = "map_o3d";
+static const std::string imageFrame = "image_frame_o3d";
 
 } /* namespace frames */
 } /* namespace o3d_slam */
