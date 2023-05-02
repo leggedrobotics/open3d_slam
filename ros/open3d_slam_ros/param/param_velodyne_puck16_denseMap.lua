fc0033795a182ecd22b2d0fce657b0ebb766796b
Juli <nubertj@ethz.ch>
2023-05-02 21:05:32 +0200

    first running version for rgb cloud

---
+++ ros/open3d_slam_ros/param/param_velodyne_puck16_denseMap.lua
@@ -0,0 +1,17 @@
+include "param_velodyne_puck16.lua"
+
+params = deepcopy(params)
+
+--MAPPER_LOCALIZER
+params.mapper_localizer.is_build_dense_map = true
+
+--MAP_BUILDER
+params.map_builder.space_carving.carve_space_every_n_scans = 10
+
+--DENSE_MAP_BUILDER
+params.dense_map_builder.map_voxel_size = 0.05
+params.dense_map_builder.scan_cropping.cropping_radius_max = 15.0
+params.dense_map_builder.space_carving.carve_space_every_n_scans = 10
+params.dense_map_builder.space_carving.truncation_distance = 0.1
+
+return params
