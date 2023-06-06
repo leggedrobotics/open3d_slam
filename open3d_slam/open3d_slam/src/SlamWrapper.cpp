111ec1946151632c7ca1ec36854332b0633ab3ac
tutuna <tutuna@leggedrobotics.com>
2023-06-06 15:54:37 +0200

    reduce after optimization sleep

--- open3d_slam/open3d_slam/src/SlamWrapper.cpp
+++ open3d_slam/open3d_slam/src/SlamWrapper.cpp
@@ -164,7 +164,7 @@ void SlamWrapper::finishProcessing() {
       }
       break;
     } else {
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
   }
   std::cout << "All submaps fnished! \n";
